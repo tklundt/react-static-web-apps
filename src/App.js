b4239969db9cda1686a63cd2f1c2545881be3d69
Trey Klundt <tklundt@asu.edu>
2023-09-19 17:41:06 -0700

    Updates2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Look 'ma it's on the web</p>
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Assets</a></li>
+        <li><a href="">Login</a></li>
+        <li><a href="">Reports</a></li>
+      </ul> 
+        <p>Look 'ma it's on the web.</p>
         <img width="25%" height="25%" src="/ACAMS_Logo.png"/>
       </header>
     </div>
